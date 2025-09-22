3196b44359768fc42b577f30df6f7778dbc18698
Ansh Ajay Mishra <Ansh@Goals-Mac-mini.local>
2025-09-22 13:36:36 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,12 @@
-
-
 import { Hero } from "@/components/pages";
-
 
 import Navbar from "../components/pages/home-page/Navbar";
 
-
-
-
 export default function Home() {
   return (
-    <main className="min-h-screen w-full bg-background text-foreground">
+    <main className="bg-background text-foreground min-h-screen w-full">
       <Navbar />
       <Hero />
- 
-   
-    
     </main>
   );
 }
